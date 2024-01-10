5df254d736be8957355185d6e96fa6b4ca24d31a
mehdi <mmmousavi.p@gmail.com>
2024-01-10 09:23:10 +0330

    unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -15,4 +15,11 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('test@testpsd.com', 'adsdas');
+    const [salt, hash] = user.password.split(',');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
